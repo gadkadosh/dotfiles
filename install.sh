ebf78aef10a6d52759d502d5bf154ac39e624e63
Gad Kadosh <32033534+gadkadosh@users.noreply.github.com>
2017-10-08 00:17:23 +0200

    [vscode] install.sh now installs the extensions. Improved version

--- install.sh
+++ install.sh
@@ -9,21 +9,48 @@ read -n 1 -p "Symlinking into home directory, this overwrites files. Proceed? " 
 echo "\ninstalling..."
 
 # Home directory dotfiles
-dotfiles_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+DOTFILES=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 dotfiles=".zshrc .bash_profile .inputrc .gitconfig"
 
 for dotfile in $dotfiles; do
-    # echo ${dotfiles_dir}/${dotfile}
-    ln -svf ${dotfiles_dir}/${dotfile} ~
+    # echo ${DOTFILES}/${dotfile}
+    ln -svf ${DOTFILES}/${dotfile} ~
 done
 
-# Vscode settings - go in Application Support on MacOS
-vscodefiles="settings.json keybindings.json snippets"
-vscode_dir=$HOME/Library/Application\ Support/Code/User
-for vscodefile in $vscodefiles; do
-    # echo ${dotfiles_dir}/vscode/${vscodefile}
-    ln -svf ${dotfiles_dir}/vscode/${vscodefile} "$vscode_dir"
-done
+# vscode settings
 
+if test "$(which code)"; then
+	if [ "$(uname -s)" = "Darwin" ]; then
+		VSCODE_HOME="$HOME/Library/Application Support/Code"
+	else
+		VSCODE_HOME="$HOME/.config/Code"
+	fi
+
+	ln -svf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
+	ln -svf "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
+	ln -svf "$DOTFILES/vscode/snippets" "$VSCODE_HOME/User/snippets"
+
+	# from `code --list-extensions`
+	modules="
+        EditorConfig.EditorConfig
+        Zignd.html-css-class-completion
+        christian-kohler.path-intellisense
+        coderfee.open-html-in-browser
+        dbaeumer.vscode-eslint
+        donjayamanne.githistory
+        donjayamanne.python
+        emmanuelbeziat.vscode-great-icons
+        qinjia.view-in-browser
+        ritwickdey.LiveServer
+        robertohuertasm.vscode-icons
+        shinnn.stylelint
+        zhuangtongfa.Material-theme
+        "
+	for module in $modules; do
+		code --install-extension "$module" || true
+	done
+fi
+
+# a few messages
 bluetext "\nBrewfile is available to install all Homebrew packages"
 bluetext "You should probably set username and email in ~/.gitconfig.local"
