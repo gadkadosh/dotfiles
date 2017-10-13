1acb36f6f2a6662a8c15c6d0b8eea761afa92e0a
Gad Kadosh <gad.kadosh@gmail.com>
2017-10-13 16:09:33 +0200

    App launching now toggles apps: hide if already running and frontmost

--- hammerspoon.symlink/launchApps.lua
+++ hammerspoon.symlink/launchApps.lua
@@ -1,20 +1,30 @@
 
 -- Setup keybindings for starting apps
 appShortcuts = {
-    {key = "h", name = "Safari"},
-    {key = "j", name = "Terminal"},
+    {key = "h", id = "Safari"},
+    {key = "j", id = "Terminal"},
     {key = "k", id = "com.microsoft.VSCode"},
-    {key = "l", name = "Finder"},
-    {key = "m", name = "Messages"},
-    {key = "p", name = "Spotify"},
+    {key = "l", id = "Finder"},
+    {key = "m", id = "Messages"},
+    {key = "p", id = "Spotify"},
 }
 
-for i, val in ipairs(appShortcuts) do
-    hs.hotkey.bind(appLaunchMeta, val.key, function()
-        if (val.id) then
-            hs.application.launchOrFocusByBundleID(val.id)
-        elseif (val.name) then
-            hs.application.launchOrFocus(val.name)
+toggleApp = function(binding)
+    return function()
+        app = hs.application.find(binding.id)
+
+        -- If it is running and is frontmost, hide
+        if app and app:isFrontmost() then
+            app:hide()
+        -- Else, if not frontmost or not running, refocus or open
+        else
+            if not hs.application.launchOrFocus(binding.id) then
+                hs.application.launchOrFocusByBundleID(binding.id)
+            end
         end
-    end)
+    end
+end
+
+for i, val in ipairs(appShortcuts) do
+    hs.hotkey.bind(appLaunchMeta, val.key, toggleApp(val))
 end
