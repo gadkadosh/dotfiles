3262430885b05a8a8b90fe358ece0e7dcae790b9
Gad Kadosh <32033534+gadkadosh@users.noreply.github.com>
2017-10-15 20:17:05 +0200

    [hammerspoon] option for each binding whether we actually want to start the app or just focus when it's already running

--- hammerspoon.symlink/launchApps.lua
+++ hammerspoon.symlink/launchApps.lua
@@ -1,13 +1,13 @@
 
 -- Setup keybindings for starting apps
 appShortcuts = {
-    { key = "h", id = "Safari" },
-    { key = "j", id = "Terminal" },
-    { key = "k", id = "com.microsoft.VSCode" },
-    { key = "l", id = "Finder" },
-    { key = "m", id = "Messages" },
-    { key = "n", id = "Activity Monitor" },
-    { key = "p", id = "Spotify" },
+    { key = "h", id = "Safari", start = true },
+    { key = "j", id = "Terminal", start = true },
+    { key = "k", id = "Visual Studio Code", start = false },
+    { key = "l", id = "Finder", start = true },
+    { key = "m", id = "Messages", start = true },
+    { key = "n", id = "Activity Monitor", start = true },
+    { key = "p", id = "Spotify", start = true },
 }
 
 toggleApp = function(binding)
@@ -17,18 +17,18 @@ toggleApp = function(binding)
         -- If it is running and is frontmost, hide
         if app and app:isFrontmost() then
             app:hide()
-
-        -- Else, if not frontmost or not running, refocus or open
-        else
-            -- If it's finder we might need more care
-            -- if app:name() == "Finder" and not app:mainWindow() then
-            --     print("is this Finder?", app, app:focusedWindow())
-            --     app:selectMenuItem({"File", "New Finder Window"})
-            if not hs.application.launchOrFocus(binding.id) then
-                hs.application.launchOrFocusByBundleID(binding.id)
-            end
+            return
         end
 
+        -- If it isn't running, do we want to start it?
+        if not app and not binding.start then
+            return
+        end
+
+        -- Else, if not frontmost or not running, refocus or open
+        if not hs.application.launchOrFocus(binding.id) then
+            hs.application.launchOrFocusByBundleID(binding.id)
+        end
     end
 end
 
