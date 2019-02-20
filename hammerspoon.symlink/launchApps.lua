317eccd59f252149d98a7587860154fd591aa3bb
Gad Kadosh <32033534+gadkadosh@users.noreply.github.com>
2019-02-20 21:34:57 +0100

    More cleaning
    Removed terminal_profiles
    Removed unused hammerspoon files - they will be there in the history of
    the repo anyways

--- hammerspoon.symlink/launchApps.lua
+++ hammerspoon.symlink/launchApps.lua
@@ -1,35 +1,20 @@
--- Setup keybindings for starting apps
+-- Keybindings for starting apps
 appShortcuts = {
-    { key = "h", app = "Safari", start = true },
-    { key = "j", app = "Terminal", start = true },
-    { key = "f", app = "Finder", start = true },
-    { key = "m", app = "Messages", start = true },
-    { key = "n", app = "Activity Monitor", start = true },
+  { key = "h", app = "Safari" },
+  { key = "j", app = "Terminal" },
+  { key = "f", app = "Finder" },
+  { key = "m", app = "Messages" },
+  { key = "n", app = "Activity Monitor" },
 }
 
-toggleApp = function(binding)
-    return function()
-        -- hs.application.enableSpotlightForNameSearches(true)
-
-        -- If it is running and is frontmost, hide
-        -- app = hs.application.find(binding.app)
-        -- if app and app:isFrontmost() then
-        --     app:hide()
-        --     return
-        -- end
-
-        -- If it isn't running, do we want to start it?
-        -- if not app and not binding.start then
-        --     return
-        -- end
-
-        -- Else, if not frontmost or not running, refocus or open
-        if not hs.application.launchOrFocus(binding.app) then
-            hs.application.launchOrFocusByBundleID(binding.app)
-        end
+toggleApp = function(app)
+  return function()
+    if not hs.application.launchOrFocus(app) then
+      hs.application.launchOrFocusByBundleID(app)
     end
+  end
 end
 
 for i, binding in ipairs(appShortcuts) do
-    hs.hotkey.bind({"alt", "shift"}, binding.key, toggleApp(binding))
+  hs.hotkey.bind({"alt", "shift"}, binding.key, toggleApp(binding.app))
 end
