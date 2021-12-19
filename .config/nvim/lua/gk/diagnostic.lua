de62ca4df5f24a074b676ac370dc6cbde1a3d962
Gad Kadosh <32033534+gadkadosh@users.noreply.github.com>
2021-12-19 12:45:48 +0100

    NVIM: add comment.lua and diagnostic.lua

---
+++ .config/nvim/lua/gk/diagnostic.lua
@@ -0,0 +1,16 @@
+vim.diagnostic.config {
+    virtual_text = { source = "if_many" },
+    float = { border = "rounded", source = "if_many" },
+    severity_sort = true,
+}
+
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+vim.api.nvim_set_keymap("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>D", "<cmd>lua vim.diagnostic.setqflist()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true })
