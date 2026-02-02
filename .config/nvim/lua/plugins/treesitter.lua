e219e7a32d802da2261b999edb5172dd0d00af12
Gad Kadosh <32033534+gadkadosh@users.noreply.github.com>
2026-02-02 13:06:18 +0100

    Fix using foldmethod and foldexpr

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -23,14 +23,12 @@ return {
             vim.api.nvim_create_autocmd("FileType", {
                 callback = function()
                     pcall(vim.treesitter.start)
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                    vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                    vim.wo.foldmethod = "expr"
+                    vim.wo.foldlevel = 99
                 end,
             })
-
-            vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-            vim.wo[0][0].foldmethod = "expr"
-            vim.o.foldlevelstart = 99
-
-            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
         end,
     },
 }
